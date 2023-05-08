79bdbe94272003ae7c17c5f971780ac6f8947308
stephen5652 <stephen5652@126.com>
2023-05-08 12:34:00 +0800

    feat: add zsh and its theme named p10k installing and configuration

---
+++ zsh/config.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+#
+home_dir=${HOME}
+
+CURRENT_DIR=$(
+	cd $(dirname $0)
+	pwd
+)
+
+safe_link() {
+	source=$1
+	des=$2
+
+	des_dir=${des%/*}
+	if [[ ! -d ${des_dir} ]]; then
+		echo -e "should make path:${des_dir}"
+		mkdir -p ${des_dir}
+	fi
+
+	if [[ -d ${des} || -f ${des} ]]; then
+		if [[ -L ${des} ]]; then # is link
+			echo -e "Warning: link is existed, we remove it:${des}"
+			rm ${des}
+		else
+
+			time=$(date "+%Y%m%d-%H%M%S")
+			echo -e "Warning: file existed ${des} , rename it to ${des}_bak_${time}"
+			mv ${des} ${des}_${time}
+		fi
+	fi
+
+	echo -e "ln -s ${source} ${des}"
+	ln -s ${source} ${des}
+}
+
+config_zsh() {
+	echo -e "\nStart config zsh:${CURRENT_DIR}"
+
+	brew install romkatv/powerlevel10k/powerlevel10k
+
+	source=${CURRENT_DIR}/zshrc
+	echo -e "\nsource:${source}"
+	dest=${home_dir}/.zshrc
+	safe_link ${source} ${dest}
+}
+
+config_p10k() {
+	echo -e "\nStart config p10k"
+	source=${CURRENT_DIR}/p10k.zsh
+	dest=${home_dir}/.p10k.zsh
+	safe_link ${source} ${dest}
+}
+
+main() {
+	config_zsh
+	config_p10k
+
+	zsh
+	source ${home_dir}/.zshrc
+}
+
+main
---
+++ zsh/install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#
+home_dir=${HOME}
+
+install_zsh() {
+	echo -e "\nStart install zsh"
+
+	echo -e "\nAfter install finish, you should run config.sh"
+
+	#install pk10
+	brew install romkatv/powerlevel10k/powerlevel10k
+
+	brew install zsh
+
+	if [[ -d ${home_dir}/.oh-my-zsh ]]; then
+		rm -rf ${home_dir}/.oh-my-zsh
+	fi
+	sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+}
+
+main() {
+	install_zsh
+}
+
+main
